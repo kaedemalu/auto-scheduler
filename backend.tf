c9ee73c7fe3008565410c248719b6b724d803781
kaedemalu <kaedemalu@gmail.com>
2020-05-17 19:42:29 +0900

    add pubsub

--- backend.tf
+++ backend.tf
@@ -1,11 +1,3 @@
-# terraform {
-#   backend "gcs" {
-#     bucket      = "shinshu-tf-state"
-#     prefix      = "state"
-#     credentials = "credentials.json"
-#   }
-# }
-
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
--- pubsub_topic.tf
+++ pubsub_topic.tf
@@ -1,3 +1,27 @@
 resource "google_pubsub_topic" "instance_sucheduler" {
   name = "instance-sucheduler"
 }
+
+resource "google_cloud_scheduler_job" "instance_stopper" {
+  name        = "instance-stopper"
+  description = "instance-stopper"
+  schedule    = "* 8 * * 1-5"
+  time_zone   = "Asia/Tokyo"
+
+  pubsub_target {
+    topic_name = google_pubsub_topic.instance_sucheduler.id
+    data       = "sample"
+  }
+}
+
+resource "google_cloud_scheduler_job" "instance_starter" {
+  name        = "instance-starter"
+  description = "instance-starter"
+  schedule    = "* 23 * * 1-5"
+  time_zone   = "Asia/Tokyo"
+
+  pubsub_target {
+    topic_name = google_pubsub_topic.instance_sucheduler.id
+    data       = "sample"
+  }
+}
