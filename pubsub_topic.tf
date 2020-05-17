d8177091a185e842f69eef2689c677dc67c1d6a6
kaedemalu <kaedemalu@gmail.com>
2020-05-17 20:42:08 +0900

    update

--- pubsub_topic.tf
+++ pubsub_topic.tf
@@ -4,26 +4,24 @@ resource "google_pubsub_topic" "instance_sucheduler" {
 
 resource "google_cloud_scheduler_job" "instance_stopper" {
   name        = "instance-stopper"
-  description = "instance-stopper"
-  schedule    = "* 8 * * 1-5"
+  schedule    = "0 8 * * 1-5"
   region      = "asia-northeast1"
   time_zone   = "Asia/Tokyo"
 
   pubsub_target {
     topic_name = google_pubsub_topic.instance_sucheduler.id
-    data       = "test"
+    data       = "{\"zone\": ${google_compute_instance.autostop_instance.zone}, \"instance\": ${google_compute_instance.autostop_instance.id}}"
   }
 }
 
 resource "google_cloud_scheduler_job" "instance_starter" {
   name        = "instance-starter"
-  description = "instance-starter"
-  schedule    = "* 23 * * 1-5"
+  schedule    = "0 23 * * 1-5"
   region      = "asia-northeast1"
   time_zone   = "Asia/Tokyo"
 
   pubsub_target {
     topic_name = google_pubsub_topic.instance_sucheduler.id
-    data       = "test"
+    data       = "{\"zone\": ${google_compute_instance.autostop_instance.zone}, \"instance\": ${google_compute_instance.autostop_instance.id}}"
   }
 }
